2bdab01dd1cfc84c95797d53b327d71b99a61ecd
Ybrayym <ybrayymjan90@gmail.com>
2019-10-31 13:26:12 -0400

    Project Complete

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Card, Icon, Image } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+
+const GITHUB_USER_INFO = "https://api.github.com/users/Ybrayym-Abamov";
+class App extends Component {
+  // constructor(props) {
+  //   super(props); // calling Component constructor 
+  //   this.state = {
+  //     user: {},                           ----------------> old way
+  //     active: false
+  //   };
+  // }
+
+  state = {
+    user: {},
+    active: false
+  };
+  handleToggle = event => {
+    if (this.state.active === true) {
+      this.setState({ active: false })
+    } 
+    else {
+    fetch(GITHUB_USER_INFO)
+      .then(res => res.json())
+      .then(user => {
+        this.setState({ user, active: true })
+      });
+    console.log(this.state.user)
+    }
+    // need more logic
+    // the DOM doesn't know what "this" is
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={this.handleToggle}>My GitHub Info</button>
+        {this.state.active === true && (
+        <Card>
+          <Image src={this.state.user.avatar_url} wrapped ui={false} />
+        <Card.Content>
+      <Card.Header>Username = {this.state.user.login}</Card.Header>
+      <Card.Meta>
+        <span className='date'>Company = {this.state.user.company}</span>
+      </Card.Meta>
+      <Card.Description>
+        Bio = {this.state.user.bio}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user'/>
+        Followers = {this.state.user.followers}
+      </a>
+    </Card.Content>
+          {/* // < div >
+          //   <h3>Username = {this.state.user.login}</h3>
+          //   <h4>Bio = {this.state.user.bio}</h4>
+          //   <h4>Followers = {this.state.user.followers}</h4>
+          //   <h4>Location = {this.state.user.location}</h4>
+          //   <h4>Company = {this.state.user.company}</h4>
+          // </div> */}
+        </Card>
+        )
+        }
+  </React.Fragment>
+    )
+  }
+}
+
+
+export default App;
+
+
+
